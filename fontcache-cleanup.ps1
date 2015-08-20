e6b5e633732f6ca7ba102b68359b96c4dc010eb4
drew <drew.hjelm@gmail.com>
2015-08-20 09:11:30 -0500

    initial commit

---
+++ fontcache-cleanup.ps1
@@ -0,0 +1,20 @@
+$svc = Get-Service fontcache
+
+net stop fontcache 
+
+$svc.WaitForStatus('Stopped','00:00:15')
+
+net start fontcache
+
+$svc.WaitForStatus('Running','00:00:15')
+
+$fontCacheFiles = Get-ChildItem "C:\Windows\ServiceProfiles\LocalService\AppData\Local\" -Filter "FontCache-S-*.dat"
+
+foreach ($file in $fontCacheFiles) { 
+	try { 
+		Remove-Item $file.fullname -Force -ErrorAction silentlyContinue; 
+	} 
+	catch {
+	continue;
+	} 
+}
