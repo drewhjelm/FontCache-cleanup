3983fe50f4a6cae2045526ff6513c3ff26b9d775
drew <drew.hjelm@gmail.com>
2015-08-20 09:57:20 -0500

    using powershell native commands for service

--- fontcache-cleanup.ps1
+++ fontcache-cleanup.ps1
@@ -1,14 +1,20 @@
+#get the Windows Font Cache Service and restart it. Don't try to restart before stopping otherwise it doesn't work well.
+
 $svc = Get-Service fontcache
 
-net stop fontcache 
+Stop-Service fontcache
 
-$svc.WaitForStatus('Stopped','00:00:15')
+$svc.WaitForStatus('Stopped')
 
-net start fontcache
+Start-Service fontcache
 
-$svc.WaitForStatus('Running','00:00:15')
+$svc.WaitForStatus('Running')
+
+#Grab only the user cache files
 
 $fontCacheFiles = Get-ChildItem "C:\Windows\ServiceProfiles\LocalService\AppData\Local\" -Filter "FontCache-S-*.dat"
+
+#delete all the files that can be deleted. Many of the files will be locked if users are still connected, so it's best to skip over them.
 
 foreach ($file in $fontCacheFiles) { 
 	try { 
@@ -18,6 +24,8 @@ foreach ($file in $fontCacheFiles) {
 	continue;
 	} 
 }
+
+#Empty the recycle bin to clear additional space.
 
 $recycleBin = (New-Object -ComObject Shell.Application).NameSpace(0xa)
 $recycleBin.items() | foreach { rm $_.path -force -recurse }
